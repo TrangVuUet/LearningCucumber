4b0a9938ad1b489e9770600a9d489ac85d3fec03
vu.thi.trang <vu.thi.trang@FRAMGIA.COM>
2017-04-18 17:01:34 +0700

    parameters

--- src/test/java/Annotation/myStepDefinition.java
+++ src/test/java/Annotation/myStepDefinition.java
@@ -1,10 +1,11 @@
 package Annotation;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -13,24 +14,41 @@ public class myStepDefinition {
 	
 	WebDriver driver;
 	
-	@Given("^I navigate to TheTestRoom\\.com$")
+	@When("^I navigate to Guru")
 	public void i_navigate_to_TheTestRoom_com() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		System.out.println("Going to TheTestRoom.com");
+		System.out.println("Going to Guru99");
 		System.setProperty("webdriver.gecko.driver", "C:/Program Files/geckodriver/geckodriver.exe");
 		driver = new FirefoxDriver();
 		driver.navigate().to("http://www.guru99.com/");
+		//driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 	  //  throw new PendingException();
 	}
 
-	@When("^I navigate to Cucumber Tutorial page$")
-	public void i_navigate_to_Cucumber_Tutorial_page() throws Throwable {
+	/*@When("^I navigate to Learn Software Testing page$")
+	public void i_navigate_to_Learn_Software_Tesing_page() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		System.out.println("Clicking on the Cucumber Tutorial Page");
-		driver.findElement(By.id("rt-logo")).click();
+		System.out.println("Clicking on the Learn Software Testing Page");
+		driver.findElement(By.partialLinkText("Learn Software Testing")).click();
 	    //throw new PendingException();
 	}
 
+	@When("^I navigate to Learn Cucumber Testing page$")
+	public void i_navigate_to_Learn_Cucumber_Tesing_page() throws Throwable {
+		System.out.println("Clicking on the Learn Cucumber Testing Page");
+		driver.findElement(By.partialLinkText("Learn Cucumber Testing")).click();
+	    // Write code here that turns the phrase above into concrete actions
+	   // throw new PendingException();
+	}*/
+	
+	@When("^I navigate to \"([^\"]*)\" page$")
+	public void i_navigate_to_page(String link) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Clicking on the Learn Testing Page");
+		driver.findElement(By.linkText(link)).click();
+	    //throw new PendingException();
+	}
+	
 	@Then("^the page title should be visible$")
 	public void the_page_title_should_be_visible() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
@@ -39,7 +57,4 @@ public class myStepDefinition {
 		driver.quit();
 	    //throw new PendingException();
 	}
-
-
-
 }
--- target/test-classes/Annotation/outline.feature
+++ target/test-classes/Annotation/outline.feature
@@ -2,6 +2,8 @@ Feature: annotation
 #This is how background can be used to eliminate duplicate steps 
 
 Scenario: TO check that the WedDriver Cucumber tutorial main page has loaded
-	Given I navigate to TheTestRoom.com
-	When I navigate to Cucumber Tutorial page
+	Given I navigate to Guru
+	When I navigate to "Learn Software Testing" page
+	When I navigate to "Learn Cucumber Testing" page
+	When I navigate to "Learn Agile Testing" page
 	Then the page title should be visible
